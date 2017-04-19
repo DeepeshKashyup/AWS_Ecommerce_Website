cb24cb8c9bb253a60fdb96dd1b73c28872926c0a
DeepeshKashyup <deepesh.kashyup@gmail.com>
2017-04-18 23:08:30 -0400

    Mongoose read changes

---
+++ shopping-cart/model/products.js
@@ -0,0 +1,14 @@
+/**
+ * Created by deepe on 4/18/2017.
+ */
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var schema = new Schema({
+    imgPath : {type : String,required:true},
+    description:{type : String,required:true},
+    title : {type : String,required:true},
+    price :{type : String,required:true}
+});
+
+module.exports = mongoose.model('Product',schema);
---
+++ shopping-cart/seed/product-seeder.js
@@ -0,0 +1,27 @@
+/**
+ * Created by deepe on 4/18/2017.
+ */
+
+var Product = require('../model/products')
+var mongoose = require('mongoose');
+mongoose.connect('localhost:27017/Shopping');
+var product = [new Product({
+    imagePath : 'https://images-na.ssl-images-amazon.com/images/I/51jBNPgWISL._SY300_QL70_.jpg',
+    title :'Apple iPhone 6 16GB Factory Unlocked GSM 4G LTE Smartphone, Gold (Certified Refurbished)',
+    description:'W',
+    price:'$282.05',
+})];
+
+var done = 0;
+for(var i = 0; i<product.length;i++){
+    product[i].save(function(err,result){
+       done++;
+       if(done==product.length){
+           exit();
+       }
+    });
+}
+
+function exit(){
+    mongoose.disconnect();
+}
